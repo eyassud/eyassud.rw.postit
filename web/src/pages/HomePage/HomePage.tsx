2555bb9810fdf957b9411f68e368f9e7155df1a1
Daniel Eyassu <danieleyassu@hotmail.com>
2024-01-06 14:33:33 -0500

    Home and Main layout created.

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,16 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <main>
+        <h1 className="">Hello next</h1>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
